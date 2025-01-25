918697343e650bc0b39cfbcbdadd4d9889881e8e
rathoreprachi05 <148968395+rathoreprachi05@users.noreply.github.com>
2025-01-25 21:49:05 +0530

    Update Game.py

--- Game.py
+++ Game.py
@@ -1,6 +1,7 @@
 list = [[1,2,3], [4,5,6], [7,8,9]]
-print(list[0])
-print(list[1])
-print(list[2])
+print(list[0][0], list[0][1], list[0][2])
+print(list[1][0], list[1][1], list[1][2])
+print(list[2][0], list[2][1], list[2][2])
+
 
 
