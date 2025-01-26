1b4e37cc6cd9e33660d8ba638479e16906c7ed50
rathoreprachi05 <148968395+rathoreprachi05@users.noreply.github.com>
2025-01-26 22:20:52 +0530

    Update Game.py

--- Game.py
+++ Game.py
@@ -1,9 +1,22 @@
-list = [[" "," "," "], [" "," "," "], [" "," "," "]]
-print(list[0][0], "|", list[0][1], "|", list[0][2])
-print("- + - + -")
-print(list[1][0], "|", list[1][1], "|", list[1][2])
-print("- + - + -")
-print(list[2][0], "|", list[2][1], "|", list[2][2])
+def print_board(list):
+  print(list[0][0], "|", list[0][1], "|", list[0][2])
+  print("- + - + -")
+  print(list[1][0], "|", list[1][1], "|", list[1][2])
+  print("- + - + -")
+  print(list[2][0], "|", list[2][1], "|", list[2][2])
 
+quit = False
+while(quit == False):
+  list = [[" "," "," "], [" "," "," "], [" "," "," "]]
+  accept = False
+  while(accept == False):
+    symbol = input("Choose your symbol('x', 'o'): ").lower()
+    if(symbol == 'x' or symbol =='o'):
+      accept = True
+    else:
+      print("Please enter either 'x' or 'o'")
+  
+
+  
 
 
