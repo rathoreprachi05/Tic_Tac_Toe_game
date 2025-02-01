3c8916aa25ce59ea1697a382a38d4f9aeb5fada8
rathoreprachi05 <148968395+rathoreprachi05@users.noreply.github.com>
2025-02-01 17:57:48 +0530

    Update Game.py

--- Game.py
+++ Game.py
@@ -10,12 +10,21 @@ while(quit == False):
   list = [[" "," "," "], [" "," "," "], [" "," "," "]]
   accept = False
   while(accept == False):
-    symbol = input("Choose your symbol('x', 'o'): ").lower()
+    p_symbol = input("Choose your symbol('x', 'o'): ").lower()
     if(symbol == 'x' or symbol =='o'):
       accept = True
     else:
       print("Please enter either 'x' or 'o'")
-  
+  if(p_symbol == 'x'):
+    c_symbol = 'o'
+  else:
+    c_symbol = 'x'
+    
+  for i in range(1,5,1):
+    user_moves()
+    computer_moves()
+    print_board(list)
+    
 
   
 
